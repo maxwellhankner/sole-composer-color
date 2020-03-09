f2daf8b459faf8b209c1c5eaab494851d92d1c09
maxwellhankner <maxwellhankner@gmail.com>
2020-03-08 20:40:59 -0500

    lots

--- assets/scripts/viewport.js
+++ assets/scripts/viewport.js
@@ -14,3 +14,19 @@ document.body.addEventListener("touchmove", function(event) {
 // document.ontouchmove = function(event){
 //     event.preventDefault();
 // }
+
+var colorButton = document.getElementById('color-button');
+var graphicButton = document.getElementById('graphic-button');
+
+var colorButtons = document.getElementById('color-buttons');
+var graphicButtons = document.getElementById('graphic-buttons');
+
+colorButton.addEventListener("click", function(){
+    colorButtons.style.display = "grid";
+    graphicButtons.style.display = "none";
+})
+
+graphicButton.addEventListener("click", function(){
+    colorButtons.style.display = "none";
+    graphicButtons.style.display = "grid";
+})
